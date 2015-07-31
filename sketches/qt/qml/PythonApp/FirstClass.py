f82265935288ae845ba346ba189d3fb2306d05f0
Jörg Preiß <joerg.preiss@slesa.de>
2015-07-31 07:27:14 +0200

    First python / c++ app

---
+++ sketches/qt/qml/PythonApp/FirstClass.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from PySide import QtWidgets
+except:
+    from PyQt5 import QtWidgets
+
+
+class FirstClass:
+    def __init__(self):
+
+    def temp:
+        a = QString("apple")
+        b = unicode("baker")
+        print a + b
