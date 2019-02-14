ab16b102eed97146c68243b1b0ee499997c54926
Jörg Preiß <joerg.preiss@slesa.de>
2019-02-14 22:30:14 +0100

    Trying i18n

--- sketches/asp/tour-of-heroes/src/app/app.module.ts
+++ sketches/asp/tour-of-heroes/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -28,7 +30,9 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService, { dataEncapsulation: false }),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'de'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+registerLocaleData(localeFr);
